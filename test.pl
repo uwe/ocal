065fb0a0a4fa84cf305bac7ca83d2c125995204d
Uwe Voelker <uwe@uwevoelker.de>
2012-12-13 23:14:10 +0100

    proper output

--- test.pl
+++ test.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+use File::Slurp;
+use Statistics::R;
 
 use lib 'lib';
 use OCal::Option;
@@ -16,5 +18,15 @@ $pos->add_leg(-2, OCal::Option->new(type => 'PUT', strike => 2050));
 
 warn Dumper $pos;
 
-print $pos->to_r;
+write_file('out.r', $pos->to_r);
+
+
+my $R = Statistics::R->new;
+#$R->run('pdf("file.pdf")');
+#$R->run('X11()');
+#$R->run('png(filename="out.png", width="1000", height="500", units="px", pointsize="12")');
+$R->run('png(filename="out.png", width=1000, height=500, units="px", pointsize=12)');
+$R->run($pos->to_r);
+
+$R->stop;
 
