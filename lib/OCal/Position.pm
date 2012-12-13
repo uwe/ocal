21e9702b2f16e2c89f6ede1236e5889d5b32dc59
Uwe Voelker <uwe@uwevoelker.de>
2012-12-13 23:14:22 +0100

    add current line

--- lib/OCal/Position.pm
+++ lib/OCal/Position.pm
@@ -23,16 +23,28 @@ vola      <- 0.3
 
 EOF
 
+    # functions for each position
     foreach my $pos (@{$self->position}) {
         $out .= $pos->[1]->to_r;
     }
 
+    # plot expiration
     $out .= 'plot(S, ';
     foreach my $pos (@{$self->position}) {
-	$out .= '+' if $pos->[0];
-        $out .= $pos->[0] . ' * ' . $pos->[1]->name_exp . '(S) ';
+        $out .= '+' . $pos->[0] . ' * ' . $pos->[1]->name_exp . '(S) ';
     }
     $out .= ', col="red", type="l", ylab="payoff", ylim=c(-10,50))';
+    $out .= "\n";
+
+    # plot current value
+    $out .= 'lines(S, ';
+    foreach my $pos (@{$self->position}) {
+        $out .= '+' . $pos->[0] . ' * ' . $pos->[1]->name . '$value[,3] ';
+    }
+    $out .= ', col=grey(.7), type="l")';
+    $out .= "\n";
+
+    ###TODO### plot other values
 
     return $out;
 }
